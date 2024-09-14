562fdc0f8e2c3c145ab280966d0e2142e49dad5e
Luis Cobo <luis.garcia@chiliz.com>
2024-09-14 15:00:49 +0200

    feat: Add github auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,52 @@
 "use client";
-import { Button, InputText, Command, CommandInput, CommandGroup, CommandEmpty, CommandItem, CommandList, CommandSeparator } from "../components/ui";
+import {
+  Button,
+  InputText,
+  Command,
+  CommandInput,
+  CommandGroup,
+  CommandEmpty,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "../components/ui";
 
 export default function Home() {
-
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex flex-col gap-4 items-center">
-          <InputText label="User" testID="innput" type="text" onChange={() => console.log('on change')} />
-          <Button variant="default" size="default" onClick={() => console.log('click')}>Click me</Button>
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
+        <div className='flex flex-col gap-4 items-center'>
+          <h1 className='text-4xl font-bold'>Welcome To CMS</h1>
+          <InputText
+            label='User'
+            testID='innput'
+            type='text'
+            onChange={() => console.log("on change")}
+          />
+          <Button
+            variant='default'
+            size='default'
+            onClick={() => console.log("click")}
+          >
+            Click me
+          </Button>
           <Command>
-            <CommandInput placeholder="Type a command or search..." />
+            <CommandInput placeholder='Type a command or search...' />
             <CommandList>
               <CommandEmpty>No results found.</CommandEmpty>
-              <CommandGroup heading="Suggestions">
+              <CommandGroup heading='Suggestions'>
                 <CommandItem>Calendar</CommandItem>
                 <CommandItem>Search Emoji</CommandItem>
                 <CommandItem>Calculator</CommandItem>
               </CommandGroup>
               <CommandSeparator />
-              <CommandGroup heading="Settings">
+              <CommandGroup heading='Settings'>
                 <CommandItem>Profile</CommandItem>
                 <CommandItem>Billing</CommandItem>
                 <CommandItem>Settings</CommandItem>
               </CommandGroup>
             </CommandList>
-        </Command>
+          </Command>
         </div>
       </main>
     </div>
