a510adfa244a9612ac426cc070090a886ba4be3b
Luis Cobo <luis.garcia@chiliz.com>
2025-01-16 12:10:42 +0100

    feat: Remove server actions & next update

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -9,13 +9,6 @@ export default function LoginForm({ searchParams }: { searchParams: Message }) {
 	return (
 		<div className="grid gap-4">
 			<form action={googleSignIn} className="flex flex-col gap-4 items-center">
-				{/* <InputText label="Email" type="email" name="email" testID="email" />
-				<InputText label="Password" type="password" name="password" testID="password" />
-
-				<Button className="w-full" formAction={signInAction} type="submit">
-					Login
-				</Button> */}
-
 				<Button variant="outline" formAction={googleSignIn} className="w-full">
 					SignIn with Google
 				</Button>
