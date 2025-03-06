4d3035a9df2e9dc93d3a69c7a1ae20a5f9ed0ad1
Luis Cobo <luis.garcia@chiliz.com>
2025-03-06 23:16:47 +0100

    feat: Add new activeStep prop to StepsBar

--- src/app/resume/create/page.tsx
+++ src/app/resume/create/page.tsx
@@ -3,7 +3,7 @@ import React, { useCallback } from 'react';
 import { ContactForm } from '../components/ContactForm';
 import { EducationForm } from '../components/EducationForm';
 import { Experience } from '../components/Experience';
-import { StepsBar, StepsBarItemsProps } from '@/app/components/StepsBar/StepsBar';
+import { StepsBar } from '@/app/components/StepsBar/StepsBar';
 import { HtmlPreviewer } from '@/app/components/HTMLPreviewer/HTMLPreviewer';
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
@@ -13,20 +13,19 @@ type UserDataStoreType = {
 	userData: UserDataType;
 	resetUserData: () => void;
 	setUserDataValue: (key: string, value: string) => void;
-};
-
-type StepsBarStoreType = {
-	items: StepsBarItemsProps[];
-	setItems: (newItems: StepsBarItemsProps[]) => void;
+	activeStep: number;
+	setActiveStep: (step: number) => void;
 };
 
 const userDataStore = create<UserDataStoreType>()(
 	persist(
 		set => ({
 			userData: defaultUserData,
+			activeStep: 0,
 			resetUserData: () => set({ userData: defaultUserData }),
 			setUserDataValue: (key: string, value: string) =>
-				set((state: UserDataStoreType) => ({ userData: { ...state.userData, [key]: value } }))
+				set((state: UserDataStoreType) => ({ userData: { ...state.userData, [key]: value } })),
+			setActiveStep: (step: number) => set({ activeStep: step })
 		}),
 		{
 			name: 'user-data-store' // unique name
@@ -34,36 +33,36 @@ const userDataStore = create<UserDataStoreType>()(
 	)
 );
 
-const stepsBarStore = create<StepsBarStoreType>()(set => ({
-	items: [
+export default function CreateResume() {
+	const userData = userDataStore((state: UserDataStoreType) => state.userData);
+	const setUserDataValue = userDataStore((state: UserDataStoreType) => state.setUserDataValue);
+	const activeStep = userDataStore((state: UserDataStoreType) => state.activeStep);
+	const setActiveStep = userDataStore((state: UserDataStoreType) => state.setActiveStep);
+	const initialSteps = [
 		{ title: 'Contact', active: true, component: ContactForm },
 		{ title: 'Experience', active: false, component: Experience },
 		{ title: 'Education', active: false, component: EducationForm },
 		{ title: 'Skills', active: false, component: ContactForm },
 		{ title: 'About', active: false, component: ContactForm },
 		{ title: 'Finish it', active: false, component: ContactForm }
-	],
-	setItems: (newItems: StepsBarItemsProps[]) => {
-		set({ items: newItems });
-	}
-}));
-
-export default function CreateResume() {
-	const setUserDataValue = userDataStore((state: UserDataStoreType) => state.setUserDataValue);
-	const stepsBarItems = stepsBarStore(state => state.items);
-	const setStepsBarItems = stepsBarStore(state => state.setItems);
-	const userData = userDataStore((state: UserDataStoreType) => state.userData);
+	];
 
 	const updateUserValue = useCallback((key: string, value: unknown) => {
 		setUserDataValue(key, value as string);
 	}, []);
 
+	const onSetNextStep = useCallback(() => {
+		if (activeStep === initialSteps.length - 1) return;
+		setActiveStep(activeStep + 1);
+	}, [activeStep, initialSteps]);
+
 	return (
 		<div className="flex h-full">
 			<div className="w-1/2 h-full">
 				<StepsBar
-					items={stepsBarItems}
-					onNextStepCallback={setStepsBarItems}
+					items={initialSteps}
+					activeStep={activeStep}
+					onNextStepCallback={onSetNextStep}
 					onFieldChangeCallback={updateUserValue}
 					initialValues={userData}
 				/>
