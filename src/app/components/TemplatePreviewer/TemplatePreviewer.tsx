7a1ebe2ec0d78de569a3131f6816758ba05ea730
Luis Cobo <luis.garcia@chiliz.com>
2025-03-14 16:35:47 +0100

    fix: Import template previewer css

--- src/app/components/TemplatePreviewer/TemplatePreviewer.tsx
+++ src/app/components/TemplatePreviewer/TemplatePreviewer.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { UserDataType } from '@/app/models/user';
-import './HTMLPreviewer.css';
+import './TemplatePreviewer.css';
 import { format } from 'date-fns';
 
 export const TemplatePreviewer = ({ userData }: { userData: UserDataType }) => {
